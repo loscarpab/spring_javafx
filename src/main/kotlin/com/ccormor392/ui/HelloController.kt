8e8c4719e449493999271ae918f9a29f88c4be31
loscarpab <ccormor392@g.educaand.es>
2024-02-05 20:48:49 +0100

    Initial commit

---
+++ src/main/kotlin/com/ccormor392/ui/HelloController.kt
@@ -0,0 +1,14 @@
+package com.ccormor392.ui
+
+import javafx.fxml.FXML
+import javafx.scene.control.Label
+
+class HelloController {
+    @FXML
+    private lateinit var welcomeText: Label
+
+    @FXML
+    private fun onHelloButtonClick() {
+        welcomeText.text = "Welcome to JavaFX Application!"
+    }
+}
